ec1f86fe1fcdcf842b14d9faf06fb5b5f3470f7d
Yael-PM <A01799842@tec.mx>
2025-09-18 21:54:52 -0600

    final commit

--- app/main.py
+++ app/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 from app.database import engine, Base
 import app.models
 from app.routers import users, enterprises, predict, login# Importación de las rutas
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Lista de orígenes permitidos
+origins = [
+    "http://localhost:5173",  
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Registrar routers
 app.include_router(users.router)
